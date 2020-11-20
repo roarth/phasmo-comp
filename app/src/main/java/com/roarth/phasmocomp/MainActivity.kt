0d166ed9e44bd886d05736ac5adcab871cfe8dc9
Mirete Benoit <ben@mirete.fr>
2020-11-20 09:19:02 +0100

    Add launcher icon & Main Toolbar

--- app/src/main/java/com/roarth/phasmocomp/MainActivity.kt
+++ app/src/main/java/com/roarth/phasmocomp/MainActivity.kt
@@ -2,10 +2,36 @@ package com.roarth.phasmocomp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    var toolbar = findViewById<Toolbar>(R.id.toolbar)
+    setSupportActionBar(toolbar)
   }
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.toolbar_main, menu)
+    return true
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+    return when(item.itemId) {
+      R.id.action_favorite -> {
+        Toast.makeText(this, "Give me a sign!", Toast.LENGTH_SHORT).show()
+        true
+      }
+      else -> super.onOptionsItemSelected(item)
+    }
+  }
+
+
+
 }
